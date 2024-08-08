e39410dcc6a8b1ba7adfde3709eed4715dad0fe9
ALAN <alanrouxel@hotmail.com>
2024-08-08 15:57:58 -0400

    modification du chemin d'accès au projet

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/main')]
+#[Route('/')]
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
--- src/Form/VoitureType.php
+++ src/Form/VoitureType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
